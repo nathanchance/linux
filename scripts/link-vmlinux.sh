8935de78926cd20fa7732e3a1ed2bb0ebcebabf3
Sami Tolvanen <samitolvanen@google.com>
2020-03-06 14:14:03 -0800

    kbuild: add support for Clang LTO
    This change adds the configuration option CONFIG_LTO_CLANG, and
    build system support for Clang's Link Time Optimization (LTO). In
    preparation for LTO support with other compilers, potentially common
    parts of the changes are gated behind CONFIG_LTO instead.
    
    With -flto, instead of object files, Clang produces LLVM bitcode,
    which is compiled into a native object at link time, allowing the
    final binary to be optimized globally. For more details, see:
    
      https://llvm.org/docs/LinkTimeOptimization.html
    
    Note that while the kernel normally uses GNU ld for linking, Clang
    requires LLD to be used instead with LTO. In order to handle bitcode,
    llvm-nm and llvm-ar must also be used instead of GNU tools:
    
      make CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm
    
    Note that DYNAMIC_FTRACE and MODVERSIONS need additional changes to
    work with LTO. These will be included in follow-up patches. KASAN is
    also excluded as enabling it with LTO isn't very useful.
    
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -52,6 +52,14 @@ modpost_link()
 		${KBUILD_VMLINUX_LIBS}				\
 		--end-group"
 
+	if [ -n "${CONFIG_LTO_CLANG}" ]; then
+		# This might take a while, so indicate that we're doing
+		# an LTO link
+		info LTO ${1}
+	else
+		info LD ${1}
+	fi
+
 	${LD} ${KBUILD_LDFLAGS} -r -o ${1} ${objects}
 }
 
@@ -76,13 +84,22 @@ vmlinux_link()
 	fi
 
 	if [ "${SRCARCH}" != "um" ]; then
-		objects="--whole-archive			\
-			${KBUILD_VMLINUX_OBJS}			\
-			--no-whole-archive			\
-			--start-group				\
-			${KBUILD_VMLINUX_LIBS}			\
-			--end-group				\
-			${@}"
+		if [ -n "${CONFIG_LTO_CLANG}" ]; then
+			# Use vmlinux.o instead of performing the slow LTO
+			# link again.
+			objects="--whole-archive		\
+				vmlinux.o 			\
+				--no-whole-archive		\
+				${@}"
+		else
+			objects="--whole-archive		\
+				${KBUILD_VMLINUX_OBJS}		\
+				--no-whole-archive		\
+				--start-group			\
+				${KBUILD_VMLINUX_LIBS}		\
+				--end-group			\
+				${@}"
+		fi
 
 		${LD} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux}	\
 			${strip_debug#-Wl,}			\
@@ -249,7 +266,6 @@ fi;
 ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init
 
 #link vmlinux.o
-info LD vmlinux.o
 modpost_link vmlinux.o
 
 # modpost vmlinux.o to check for section mismatches
