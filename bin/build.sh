5577655aee926053e6e6441333da146df7c1dc7e
Nathan Chancellor <natechancellor@gmail.com>
2020-05-11 23:53:28 -0700

    build.sh: Initial revision
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

---
+++ bin/build.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BASE=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+BASE=${BASE%/*}
+
+# Get parameters
+function parse_parameters() {
+    MY_TARGETS=()
+    while ((${#})); do
+        case ${1} in
+            */ | *.o | vmlinux | zImage | modules) MY_TARGETS=("${MY_TARGETS[@]}" "${1}") ;;
+            *=*) export "${1?}" ;;
+            -i | --incremental) INCREMENTAL=true ;;
+            -j | --jobs) JOBS=${1} ;;
+            -u | --update-config-only) UPDATE_CONFIG_ONLY=true ;;
+            -v | --verbose) VERBOSE=true ;;
+        esac
+        shift
+    done
+    [[ -z ${MY_TARGETS[*]} ]] && MY_TARGETS=(all modules_install)
+
+    # Handle architecture specific variables
+    case ${ARCH:=arm} in
+        arm)
+            CROSS_COMPILE=arm-linux-gnueabi-
+            CONFIG=arch/arm/configs/bcm2709_defconfig
+            KERNEL_IMAGE=zImage
+            ;;
+
+        # We only support arm at this point but that might change eventually
+        *)
+            echo "\${ARCH} value of '${ARCH}' is not supported!" 2>&1
+            exit 22
+            ;;
+    esac
+}
+
+function set_toolchain() {
+    # Add toolchain folders to PATH and request path override (PO environment variable)
+    export PATH="${PO:+${PO}:}${CBL_LLVM:+${CBL_LLVM}:}${BASE}/bin:${CBL_BNTL:+${CBL_BNTL}:}${PATH}"
+
+    # Use ccache if it exists
+    CCACHE=$(command -v ccache)
+
+    # Set default values if user did not supply them above
+    true \
+        "${AS:=${CROSS_COMPILE}as}" \
+        "${AR:=llvm-ar}" \
+        "${CC:=${CCACHE:+ccache }clang}" \
+        "${HOSTAR:=llvm-ar}" \
+        "${HOSTCC:=${CCACHE:+ccache }clang}" \
+        "${HOSTLD:=ld.lld}" \
+        "${HOSTLDFLAGS:=-fuse-ld=lld}" \
+        "${JOBS:="$(nproc)"}" \
+        "${LD:=ld.lld}" \
+        "${NM:=llvm-nm}" \
+        "${O:=${BASE}/out/${ARCH}}" \
+        "${OBJCOPY:=llvm-objcopy}" \
+        "${OBJDUMP:=llvm-objdump}" \
+        "${OBJSIZE:=llvm-size}" \
+        "${STRIP:=llvm-strip}"
+
+    printf '\n\e[01;32mToolchain location:\e[0m %s\n\n' "$(dirname "$(command -v "${CC##* }")")"
+    printf '\e[01;32mToolchain version:\e[0m %s \n\n' "$("${CC##* }" --version | head -n1)"
+}
+
+function kmake() {
+    set -x
+    time make \
+        -C "${BASE}" \
+        -j"${JOBS}" \
+        ${COND_MAKE_ARGS:+"${COND_MAKE_ARGS[@]}"} \
+        AS="${AS}" \
+        AR="${AR}" \
+        ARCH="${ARCH}" \
+        CC="${CC}" \
+        ${CROSS_COMPILE:+CROSS_COMPILE="${CROSS_COMPILE}"} \
+        HOSTAR="${AR}" \
+        HOSTCC="${HOSTCC}" \
+        HOSTLD="${HOSTLD}" \
+        HOSTLDFLAGS="${HOSTLDFLAGS}" \
+        INSTALL_MOD_PATH=rootfs \
+        LD="${LD}" \
+        NM="${NM}" \
+        O="$(realpath -m --relative-to="${BASE}" "${O}")" \
+        OBJCOPY="${OBJCOPY}" \
+        OBJDUMP="${OBJDUMP}" \
+        OBJSIZE="${OBJSIZE}" \
+        STRIP="${STRIP}" \
+        ${V:+V=${V}} \
+        "${@}"
+    set +x
+}
+
+function build_kernel() {
+    # Build silently by default
+    ${VERBOSE:=false} || COND_MAKE_ARGS=(-s)
+
+    # Build list of configure targets
+    CONFIG_MAKE_TARGETS=("${CONFIG##*/}")
+    ${INCREMENTAL:=false} || CONFIG_MAKE_TARGETS=(distclean "${CONFIG_MAKE_TARGETS[@]}")
+
+    # Build list of build targets
+    ${UPDATE_CONFIG_ONLY:=false} && FINAL_MAKE_TARGETS=(savedefconfig)
+    [[ -z ${FINAL_MAKE_TARGETS[*]} ]] && FINAL_MAKE_TARGETS=("${MY_TARGETS[@]}")
+
+    # Build the kernel with targets
+    kmake "${CONFIG_MAKE_TARGETS[@]}" "${FINAL_MAKE_TARGETS[@]}"
+
+    # Copy over new config if needed
+    if ${UPDATE_CONFIG_ONLY}; then
+        cp -v "${O}"/defconfig "${BASE}"/${CONFIG}
+        exit 0
+    fi
+
+    # Let the user know where the kernel will be (if we built one)
+    KERNEL=$(readlink -f "${O}")/arch/${ARCH}/boot/${KERNEL_IMAGE}
+    [[ -f ${KERNEL} ]] && printf '\n\e[01;32mKernel is now available at:\e[0m %s\n' "${KERNEL}"
+}
+
+parse_parameters "${@}"
+set_toolchain
+build_kernel
